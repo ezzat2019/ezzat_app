b60ee31ba1fb080624e86187d4bd273460e6be60
ezzat <coezz@yahoo.com>
2025-10-27 17:36:49 +0300

    enhance gridview

--- .firebase/unittest-daadf/hosting/flutter_bootstrap.js
+++ .firebase/unittest-daadf/hosting/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1671114433"
+    serviceWorkerVersion: "1272849451"
   }
 });
